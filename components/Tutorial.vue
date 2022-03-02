931e167536f51ff3ccfaf5b8c5f08177f1d96918
Maratib Ali Khan <maratib@gmail.com>
2022-03-02 23:22:29 +0500

    initial commit

---
+++ components/Tutorial.vue
@@ -0,0 +1,12 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <h1 class="text-3xl text-red-600 font-bold underline">Hello world!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NuxtTutorial",
+};
+</script>
